d726e1a530b9dd5aaee7785585a33cad429febe1
mikul233 <kulagina.maria2006@gmail.com>
2025-10-24 16:43:32 +0300

    структура

---
+++ src/main/java/com/mikul223/todolist/Main.java
@@ -0,0 +1,7 @@
+package com.mikul223.todolist;
+public class Main{
+    public static void main(String[] args){
+        System.out.println("Todo List Application Started!");
+        System.out.println("Project setup successfully!");
+    }
+}
