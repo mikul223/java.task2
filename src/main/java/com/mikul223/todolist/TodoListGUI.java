ca42beadcb37ddbd3e9ce022f9206eaec3af12f9
mikul233 <kulagina.maria2006@gmail.com>
2025-10-25 21:12:46 +0300

    +table

--- src/main/java/com/mikul223/todolist/TodoListGUI.java
+++ src/main/java/com/mikul223/todolist/TodoListGUI.java
@@ -2,6 +2,9 @@ package com.mikul223.todolist;
 
 import javax.swing.*;
 import java.awt.*;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.ListSelectionModel;
+
 
 public class TodoListGUI {
     private JFrame mainFrame;
@@ -28,6 +31,41 @@ public class TodoListGUI {
         titleLabel.setFont(new Font("Arial", Font.BOLD, 20));
         mainPanel.add(titleLabel, BorderLayout.NORTH);
 
+        // Таблица с задачами
+        String[] columnNames = {"ID", "Название", "Описание", "Статус", "Приоритет",
+                "Категория", "Дедлайн", "Просрочена?"
+        };
+
+        // Модель таблицы
+        DefaultTableModel tableModel = new DefaultTableModel(columnNames, 40) {
+            @Override
+            public boolean isCellEditable(int row, int column) {
+                return false; // Редактирование в таблице запрещено
+            }
+        };
+
+        JTable tasksTable = new JTable(tableModel);
+
+        // Настраиваем внешний вид таблицы
+        tasksTable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        tasksTable.getTableHeader().setReorderingAllowed(false);
+        tasksTable.setRowHeight(25);
+
+        // Настраиваем ширину колонок
+        tasksTable.getColumnModel().getColumn(0).setPreferredWidth(40);  // ID
+        tasksTable.getColumnModel().getColumn(1).setPreferredWidth(150); // Название
+        tasksTable.getColumnModel().getColumn(2).setPreferredWidth(200); // Описание
+        tasksTable.getColumnModel().getColumn(3).setPreferredWidth(80);  // Статус
+        tasksTable.getColumnModel().getColumn(4).setPreferredWidth(80);  // Приоритет
+        tasksTable.getColumnModel().getColumn(5).setPreferredWidth(80); // Категория
+        tasksTable.getColumnModel().getColumn(6).setPreferredWidth(80); // Дедлайн
+        tasksTable.getColumnModel().getColumn(7).setPreferredWidth(80);  // Просрочена?
+
+        // Таблица прокручиваема
+        JScrollPane tableScrollPane = new JScrollPane(tasksTable);
+        tableScrollPane.setPreferredSize(new Dimension(700, 400));
+
+        mainPanel.add(tableScrollPane, BorderLayout.CENTER);
 
         // Панель с кнопками внизу
         JPanel bottomPanel = new JPanel(new FlowLayout());
